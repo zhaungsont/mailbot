be110c639825e4131cba4ea495bfe74c890f193f
Michael Chuang <81564341+zhaungsont@users.noreply.github.com>
2021-11-15 13:47:50 +0800

    Sever Idle Fix #2

--- app.js
+++ app.js
@@ -183,11 +183,11 @@ console.log(time);
   });
 });
 app.get("/", function(req,res){
-  res.send("Sakana's Mailbot");
+  res.sendFile(__dirname + "/index.html")
 })
 
 setInterval(function() {
-    http.get("http://sakana-mailbot.herokuapp.com");
+    https.get("http://sakana-mailbot.herokuapp.com");
 }, 300000); // every 5 minutes (300000)
 
 app.listen(process.env.PORT || 3000, function(){
